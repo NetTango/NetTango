a980428a25237f351ad92312fe44324c5d3c5011
Michael Horn <michael-horn@northwestern.edu>
2014-10-18 10:54:58 -0500

    initial checkin

---
+++ lib/ntango.dart
@@ -0,0 +1,45 @@
+/*
+ * NetTango
+ * Copyright (c) 2014 Michael S. Horn, Uri Wilensky, and Corey Brady
+ * 
+ * Northwestern University
+ * 2120 Campus Drive
+ * Evanston, IL 60613
+ * http://tidal.northwestern.edu
+ * http://ccl.northwestern.edu
+ 
+ * This project was funded in part by the National Science Foundation.
+ * Any opinions, findings and conclusions or recommendations expressed in this
+ * material are those of the author(s) and do not necessarily reflect the views
+ * of the National Science Foundation (NSF).
+ */
+library NetTango;
+
+import 'dart:math';
+import 'dart:async';
+import 'dart:html';
+import 'dart:web_audio';
+
+part 'src/model/agent.dart';
+part 'src/model/aset.dart';
+part 'src/model/color.dart';
+part 'src/model/model.dart';
+part 'src/model/patch.dart';
+part 'src/model/program.dart';
+part 'src/model/turtle.dart';
+
+part 'src/utils/matrix.dart';
+part 'src/utils/sounds.dart';
+part 'src/utils/touch.dart';
+part 'src/utils/utils.dart';
+part 'src/utils/tween.dart';
+
+part 'src/blocks/block.dart';
+part 'src/blocks/bug.dart';
+part 'src/blocks/control.dart';
+part 'src/blocks/if.dart';
+part 'src/blocks/menu.dart';
+part 'src/blocks/parameter.dart';
+part 'src/blocks/runtime.dart';
+part 'src/blocks/start.dart';
+part 'src/blocks/workspace.dart';
