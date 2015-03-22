cd4a39f1251f682563593f2b2a8747a43cab7f79
Michael Horn <michael-horn@northwestern.edu>
2015-03-22 15:43:39 -0500

    added extra crunchy and nutricious beetles

--- example/FrogPond/web/dart/fly.dart
+++ example/FrogPond/web/dart/fly.dart
@@ -26,7 +26,7 @@ part of FrogPond2;
 class Fly extends Turtle {
   
   
-  /* pond that contains this frog */
+  /* pond that contains this fly */
   FrogPond pond;  
 
   /* flies make lazy circles around the pond */
@@ -44,8 +44,20 @@ class Fly extends Turtle {
     this.img.src = "images/dragonfly.png";
     this.x = pond.minWorldX + Agent.rnd.nextDouble() * pond.worldWidth;
     this.y = pond.minWorldY + Agent.rnd.nextDouble() * pond.worldHeight;
-    this.size = 0.3;
+    this.size = 0.2;
   }
+
+
+  Fly.withPosition(FrogPond pond, num x, num y) : super(pond) {
+    this.pond = pond;
+    this.img.src = "images/dragonfly.png";
+    this.x = x;
+    this.y = y;
+    this.size = 0.2;
+  }
+
+
+  num get energy => pond["energy-gain"];
   
   
   Fly hatch() {
@@ -73,7 +85,7 @@ class Fly extends Turtle {
   
   
   void tick() {
-    forward(size * 0.2);
+    forward(size * 0.1);
     left(_turn);
     if (Agent.rnd.nextInt(100) > 98) {
       _turn = Agent.rnd.nextDouble() * 6.0 - 3.0;
@@ -85,6 +97,40 @@ class Fly extends Turtle {
     if (dead) return;
     num iw = size;
     num ih = size;
+    ctx.drawImageScaled(img, -iw/2, -ih/2, iw, ih);
+  }
+}
+
+
+class Beetle extends Fly {
+
+  Beetle(FrogPond pond) : super(pond) {
+    this.size = 0.6;
+    this.img.src = "images/beetle_green0.png";
+  }
+
+  num get energy => pond["energy-gain"] * 10;
+
+  Beetle hatch() {
+    Beetle clone = new Beetle(pond);
+    copyTo(clone);
+    return clone;
+  }
+
+
+  void tick() {
+    forward(size * 0.2);
+    left(_turn);
+    if (Agent.rnd.nextInt(100) > 98) {
+      _turn = Agent.rnd.nextDouble() * 3.0 - 1.5;
+    }
+  }
+
+
+  void draw(CanvasRenderingContext2D ctx) {
+    if (dead) return;
+    num iw = size * img.width * 0.03;
+    num ih = size * img.height * 0.03;
     ctx.drawImageScaled(img, -iw/2, -ih/2, iw, ih);
   }
 }
