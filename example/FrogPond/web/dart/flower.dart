ecd76983b8ac121b2ecf35108e8c915009ee6e35
Michael Horn <michael-horn@northwestern.edu>
2015-03-22 15:47:29 -0500

    initial

---
+++ example/FrogPond/web/dart/flower.dart
@@ -0,0 +1,50 @@
+/*
+ * Frog Pond Evolution
+ * Copyright (c) 2015 Michael S. Horn
+ * 
+ *           Michael S. Horn (michael-horn@northwestern.edu)
+ *           Northwestern University
+ *           2120 Campus Drive
+ *           Evanston, IL 60613
+ *           http://tidal.northwestern.edu
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+part of FrogPond2;
+
+
+class Flower extends Turtle {
+  
+  ImageElement img = new ImageElement();
+  
+  
+  Flower(FrogPond pond, num x, num y) : super(pond) {
+    img.src = "images/lily.png";
+    this.x = x;
+    this.y = y;
+    this.size = 1.75;
+  }
+  
+
+  void draw(CanvasRenderingContext2D ctx) {  
+    num iw = size;
+    num ih = size;
+    ctx.drawImageScaled(img, -iw/2, -ih/2, iw, ih);
+  }
+  
+  
+  Turtle hatch() {
+    return null;
+  }
+}  
