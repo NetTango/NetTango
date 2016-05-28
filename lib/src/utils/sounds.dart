1ccf3a6c6e85f8e0a29959b33c09fca6240bad7b
Michael Horn <michael-horn@northwestern.edu>
2016-05-27 20:34:20 -0500

    updated to new audio api

--- lib/src/utils/sounds.dart
+++ lib/src/utils/sounds.dart
@@ -1,6 +1,6 @@
 /*
  * NetTango
- * Copyright (c) 2014 Michael S. Horn, Uri Wilensky, and Corey Brady
+ * Copyright (c) 2016 Michael S. Horn, Uri Wilensky, and Corey Brady
  * 
  * Northwestern University
  * 2120 Campus Drive
@@ -24,59 +24,28 @@ class Sounds {
 
 
   static void loadSound(String name, [String dir = "sounds"]) {
-
-    /*
-    AudioElement audio = new AudioElement();
-    audio.src = "$dir/$name.wav";
-    sounds[name] = audio;
-    */
     String url = "$dir/$name.wav";
     HttpRequest request = new HttpRequest();
     request.open("GET", url, async: true);
     request.responseType = "arraybuffer";
     request.onLoad.listen((e) {
       audio.decodeAudioData(request.response).then((AudioBuffer buffer) {
-        if (buffer == null) {
-          return;
-        }
-        sounds[name] = buffer;
+        if (buffer != null) sounds[name] = buffer;
       });      
     });
     request.onError.listen((e) => print("BufferLoader: XHR error"));
     request.send();
-
-    /*
-    HttpRequest http = new HttpRequest();
-    http.responseType = "arraybuffer";
-    http.onLoad.listen((e) {
-      audio.decodeAudioData(
-          http.response,
-          (buffer) { sounds[name] = buffer; },
-          (err) => print(err));
-    });
-    http.open('GET', "$dir/$name.wav");
-    http.send();
-    */
-
   }
 
 
   static void playSound(String name) {
-    /*
     if (sounds[name] != null && !mute) {
-      sounds[name].volume = 0.4;
-      sounds[name].play();
+      AudioBufferSourceNode source = audio.createBufferSource();
+      source.buffer = sounds[name];
+      source.connectNode(audio.destination, 0, 0);
+      source.loop = false;
+      source.playbackRate.value = 1;
+      source.start(0);
     }
-    */
-
-    if (sounds[name] == null) return;
-    AudioBufferSourceNode source = audio.createBufferSource();
-    source.buffer = sounds[name];
-    source.connectNode(audio.destination, 0, 0);
-    source.loop = false;
-    source.playbackRate.value = 1;
-    source.start(0);
-
   }
-
 }
