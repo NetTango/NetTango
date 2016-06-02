fa10a36f92a90f27050a515b0a45decebbe7b26d
Michael Horn <michael-horn@northwestern.edu>
2016-06-01 22:02:27 -0500

    added runtime interface to breed

--- lib/src/model/breed.dart
+++ lib/src/model/breed.dart
@@ -1,6 +1,6 @@
 /*
  * NetTango
- * Copyright (c) 2014 Michael S. Horn, Uri Wilensky, and Corey Brady
+ * Copyright (c) 2016 Michael S. Horn, Uri Wilensky, and Corey Brady
  * 
  * Northwestern University
  * 2120 Campus Drive
@@ -17,9 +17,10 @@ part of NetTango;
 
 
 /**
- * Breed is a special AgentSet
+ * Breed is a special AgentSet that also has a reference to a CodeWorkspace for 
+ * programming agents in this breed.
  */
-class Breed extends AgentSet {
+class Breed extends AgentSet with Runtime {
 
 /**
  * All turtles in this breed will run programs from this workspace
@@ -28,7 +29,10 @@ class Breed extends AgentSet {
 
 
   Breed([CodeWorkspace workspace = null]) {
-    this.workspace = workspace;
+    if (workspace != null ) {
+      this.workspace = workspace;
+      workspace.runtime = this;
+    }
   }
 
   
@@ -41,4 +45,44 @@ class Breed extends AgentSet {
       t.program = new Program(workspace.start, t);
     }
   }
+
+
+// ------------ RUNTIME INTERFACE -------------------------
+
+/**
+ * Just do the normal agent set tick, but allow for fast
+ * forwarding. Also, don't tick if play_state <= 0.
+ */
+  void tick() {
+    if (play_state > 0) {
+      for (int i=0; i<play_state; i++) {
+        super.tick();
+      }
+    }
+  }
+
+
+/**
+ * Step forward 1 tick (step forward button pressed)
+ */
+  void stepForward() {
+    tick();
+  }
+
+   
+/**
+ * Called when the restart button is pressed in a workspace
+ */
+  void setup() {
+    restartProgram();
+  }
+
+  
+/**
+ * Called whenever the blocks program is changed
+ */
+  void programChanged() {
+    restartProgram();
+  }
+
 }
