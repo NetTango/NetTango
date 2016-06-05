e0c06f20560370a0be885344995eb28926679e06
Michael Horn <michael-horn@northwestern.edu>
2016-06-04 21:45:41 -0500

    flocking is working

--- examples/Flocking/web/dart/main.dart
+++ examples/Flocking/web/dart/main.dart
@@ -26,7 +26,7 @@ library Flocking;
 import 'dart:js';
 //import 'dart:async';
 import 'dart:html';
-//import 'dart:math';
+import 'dart:math';
 //import 'dart:convert';
 import 'package:NetTango/ntango.dart';
 
@@ -34,20 +34,22 @@ import 'package:NetTango/ntango.dart';
 class GreenBird extends BlueBird {
 
   GreenBird(Model model) : super(model) {
-    color = new Color(10, 200, 10, 200);
+    color = new Color(10, 200, 10, 100 + Agent.rnd.nextInt(100));
     size = 0.75;
   }
 }
 
 
 class BlueBird extends Turtle {
+
+  AgentSet flockmates = new AgentSet();
   
   BlueBird(Model model) : super(model) {
-    color = new Color(10, 10, 100, 200);
+    color = new Color(10, 10, 100, 100 + Agent.rnd.nextInt(100));
     size = 0.75;
   }
   
-  
+
   void draw(CanvasRenderingContext2D ctx) {
     ctx.fillStyle = color.toString();
     ctx.beginPath();
@@ -59,14 +61,55 @@ class BlueBird extends Turtle {
     //ctx.arc(0, 0, radius, 0, PI * 2, true);
     ctx.fill();
   }
+
+
+  AgentSet findFlockmates(num radius) {
+    //forward(radius * 0.6);
+    num tx = x, ty = y;
+    //backward(radius * 0.6);
+    return model.getBreed(this.runtimeType).getTurtlesInRadius(tx, ty, radius);
+  }
+
+
+  Turtle getClosestBird() {
+    //forward(radius * 0.6);
+    //AgentSet aset = model.getBreed(this.runtimeType).getTurtlesInRadius(x, y, radius);
+    //backward(radius * 0.6);
+    num mindist = 100;
+    Turtle closest = null;
+    if (flockmates != null) {
+      for (int i=0; i<flockmates.length; i++) {
+        if (flockmates[i] != this) {
+          num dist = distanceBetween(flockmates[i]);
+          if (closest == null || dist < mindist) {
+            mindist = dist;
+            closest = flockmates[i];
+          }
+        }
+      }
+    }
+    return closest;
+  }
+
   
   dynamic doAction(String action, List params) {
     switch(action) {
       case "forward": return doForward(params[0]);
       case "left": return doLeft(params[0]);
       case "right": return doRight(params[0]);
+      case "towards": return doTurnTowards(params[0]);
+      case "align": return doAlign(params[0]);
+      case "away": return doTurnAway(params[0]);
+      case "if-closer": return ifCloser(params[0]);
+      case "start": flockmates = findFlockmates(3); return null;
     }
     return null;
+  }
+
+
+  bool ifCloser(param) {
+    Turtle t = getClosestBird();
+    return (t != null) ? (distanceBetween(getClosestBird()) <= param) : false;
   }
 
   
@@ -85,7 +128,80 @@ class BlueBird extends Turtle {
   void doRight(param) {
     right( (param is num) ? param : Agent.rnd.nextInt(10));
   }
+
+
+  void doTurnAway(param) {
+    if (param is num) {
+      Turtle t = getClosestBird();
+      if (t != null) {
+        num delta = headingDifference(t);
+        if (delta >= 0) {
+          right(min(delta, param));
+        } else {
+          right(max(delta, -param));
+        }
+      }
+    }
+  }
+
+
+  void doAlign(param) {
+    if (param is num && flockmates != null) {
+      num delta = 0;
+      num count = 0;
+      for (int i=0; i<flockmates.length; i++) {
+        if (flockmates[i] != this) {
+          delta += headingDifference(flockmates[i]);
+          count++;
+        }
+      }
+      if (count > 0) {
+        delta /= count; // average heading difference
+        if (delta >= 0) {
+          left(min(delta, param));
+        } else {
+          left(max(delta, -param));
+        }
+      }
+    }
+  }
+
+
+  void doTurnTowards(param) {
+    if (param is num && flockmates != null) {
+      num delta = 0;
+      num count = 0;
+      for (int i=0; i<flockmates.length; i++) {
+        if (flockmates[i] != this) {
+          delta += angleBetween(flockmates[i]);
+          count++;
+        }
+      }
+      if (count > 0) {
+        delta /= count; // average heading difference
+        if (delta >= 0) {
+          left(min(delta, param));
+        } else {
+          left(max(delta, -param));
+        }
+      }
+    }
+    /*
+    if (param is num) {
+      Turtle t = getClosestBird();
+      if (t != null) {
+        num delta = headingDifference(t);
+        if (delta > 0) {
+          left(min(delta, param));
+        } else {
+          right(max(delta, param));
+        }
+      }
+    }
+    */
+  }
   
+
   Turtle hatch() {
     return new BlueBird(model);
   }
@@ -113,12 +229,12 @@ class FlockModel extends Model {
   void breedSetup(Breed breed) {
     clearTurtles(breed);
     if (breed.turtleType == BlueBird) {
-      for (int i=0; i<50; i++) {
+      for (int i=0; i<100; i++) {
         addTurtle(new BlueBird(this) .. setXY(-14, 0));
       }
     }
     else {
-      for (int i=0; i<50; i++) {
+      for (int i=0; i<100; i++) {
         addTurtle(new GreenBird(this) .. setXY(14, 0));
       }
     }
@@ -175,8 +291,8 @@ void main() {
           {
             "type" : "range",
             "min" : 0,
-            "max" : 10,
-            "step" : 1,
+            "max" : 20,
+            "step" : 0.5,
             "default" : 3,
             "random" : true,
             "label" : "degrees"
@@ -193,8 +309,8 @@ void main() {
           {
             "type" : "range",
             "min" : 0,
-            "max" : 10,
-            "step" : 1,
+            "max" : 20,
+            "step" : 0.5,
             "default" : 3,
             "random" : true,
             "label" : "degrees"
@@ -202,6 +318,76 @@ void main() {
         ]
       },
 
+      {
+        "name" : "turn towards",
+        "short" : "towards",
+        "instances" : 2,
+        "action" : "towards",
+        "params" : [
+          {
+            "type" : "range",
+            "min" : 0,
+            "max" : 20,
+            "step" : 0.25,
+            "default" : 3,
+            "label" : "max degrees"
+ 
+          }
+        ]
+      },
+
+      {
+        "name" : "turn away from",
+        "short" : "away",
+        "instances" : 2,
+        "action" : "away",
+        "params" : [
+          {
+            "type" : "range",
+            "min" : 0,
+            "max" : 20,
+            "step" : 0.25,
+            "default" : 1.5,
+            "label" : "max degrees"
+          }
+        ]
+      },
+
+      {
+        "name" : "align with",
+        "short" : "align",
+        "instances" : 2,
+        "action" : "align",
+        "params" : [
+          {
+            "type" : "range",
+            "min" : 0,
+            "max" : 20,
+            "step" : 0.25,
+            "default" : 3,
+            "label" : "max degrees"
+          }
+        ]
+      },
+
+      {
+        "name" : "if bird closer than",
+        "short" : "if",
+        "type" : "if-else",
+        "action" : "if-closer",
+        "params" : [
+          {
+            "type" : "range",
+            "min" : 0,
+            "max" : 5,
+            "step" : 0.25,
+            "default" : 1,
+            "label" : "degrees"
+          }
+        ]
+
+      }
+/*
       {
         "name" : "waggle",
         "color" : "blue",
@@ -212,6 +398,7 @@ void main() {
           }
         ]
       }
+*/      
     ]
   };
 
