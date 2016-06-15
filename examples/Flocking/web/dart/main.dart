5c1796c5f260eb3d5407861ed50b3d9906c79335
Michael Horn <michael-horn@northwestern.edu>
2016-06-14 21:51:53 -0500

    disable fastforward and stepforward for flocking model

--- examples/Flocking/web/dart/main.dart
+++ examples/Flocking/web/dart/main.dart
@@ -1,25 +1,5 @@
 /*
- * Frog Pond Evolution
- * Copyright (c) 2015 Michael S. Horn
- * 
- *           Michael S. Horn (michael-horn@northwestern.edu)
- *           Northwestern University
- *           2120 Campus Drive
- *           Evanston, IL 60613
- *           http://tidal.northwestern.edu
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * NetTango Flocking
  */
 library Flocking;
 
@@ -264,6 +244,8 @@ void main() {
     "anchor" : "left",
     "color" : "rgba(0, 0, 0, 0.6)",
     "defaultProgram" : "forward(0.3);left(1);",
+    "fastForwardButton" : false,
+    "stepForwardButton" : false,
     "blocks" : [
       {
         "name" : "forward",
