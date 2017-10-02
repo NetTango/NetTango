dce0d0f121a287031c5238e1264f9087958dd25d
Michael Horn <michael-horn@northwestern.edu>
2017-10-02 14:41:30 -0500

    no longer supporting the modeling environment -- just the language

--- lib/ntango.dart
+++ lib/ntango.dart
@@ -1,6 +1,6 @@
 /*
  * NetTango
- * Copyright (c) 2014 Michael S. Horn, Uri Wilensky, and Corey Brady
+ * Copyright (c) 2017 Michael S. Horn, Uri Wilensky, and Corey Brady
  * 
  * Northwestern University
  * 2120 Campus Drive
@@ -19,30 +19,14 @@ import 'dart:math';
 import 'dart:html';
 import 'dart:web_audio';
 
-part 'src/model/agent.dart';
-part 'src/model/aset.dart';
-part 'src/model/breed.dart';
-part 'src/model/color.dart';
-part 'src/model/model.dart';
-part 'src/model/patch.dart';
-part 'src/model/turtle.dart';
-
 part 'src/utils/matrix.dart';
 part 'src/utils/sounds.dart';
 part 'src/utils/touch.dart';
-part 'src/utils/utils.dart';
 part 'src/utils/tween.dart';
+part 'src/utils/utils.dart';
 
 part 'src/blocks/block.dart';
-part 'src/blocks/bug.dart';
-part 'src/blocks/chance.dart';
 part 'src/blocks/control.dart';
-part 'src/blocks/if.dart';
 part 'src/blocks/menu.dart';
 part 'src/blocks/parameter.dart';
-part 'src/blocks/program.dart';
-part 'src/blocks/range.dart';
-part 'src/blocks/repeat.dart';
-part 'src/blocks/runtime.dart';
-part 'src/blocks/start.dart';
 part 'src/blocks/workspace.dart';
