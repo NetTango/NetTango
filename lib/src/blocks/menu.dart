09f42a120d87fc1a5bf42d13d53e0aa18d90f64c
Michael Horn <michael-horn@northwestern.edu>
2017-10-02 14:42:44 -0500

    overhaul for multi-start environment

--- lib/src/blocks/menu.dart
+++ lib/src/blocks/menu.dart
@@ -1,6 +1,6 @@
 /*
  * NetTango
- * Copyright (c) 2014 Michael S. Horn, Uri Wilensky, and Corey Brady
+ * Copyright (c) 2017 Michael S. Horn, Uri Wilensky, and Corey Brady
  * 
  * Northwestern University
  * 2120 Campus Drive
@@ -19,45 +19,33 @@ part of NetTango;
 /**
  * Visual programming menu bar
  */
-class Menu {
+class BlockMenu {
 
-  /* Link back to the code workspace that owns this menu bar */
+  /// Link back to the code workspace that owns this menu bar
   CodeWorkspace workspace;
-  
-  /* Dimensions of the menu */
-  num x, y, w, h;
-  
-  /* Slots for programming blocks */
+
+  /// Slots for programming blocks
   List<Slot> slots = new List<Slot>();
 
-  /* VCR Buttons */
-  List<VCRButton> _buttons = new List<VCRButton>();
+  /// Menu background color
+  String color = "rgba(0,0,0, 0.2)";
 
-  /* Special VCR button play/pause: references _buttons[0] */
-  VCRButton playButton, fastForwardButton, stepForwardButton;
+  /// gets resized automatically
+  num width = BLOCK_WIDTH + BLOCK_GUTTER * 2;
+  
+  
+  BlockMenu(this.workspace);
 
-  /* Menu background color */
-  String background = "rgba(0, 0, 0, 0.3)";
-  
-  
-  Menu(this.workspace, this.x, this.y, this.w, this.h) {
-    _buttons.add(new VCRButton(this, h - 24, VCRButtonShape.Play));
-    _buttons.add(new VCRButton(this, h - 24, VCRButtonShape.FastForward));
-    _buttons.add(new VCRButton(this, h - 24, VCRButtonShape.StepForward));
-    _buttons.add(new VCRButton(this, h - 24, VCRButtonShape.Restart));
-    playButton = _buttons[0];
-    fastForwardButton = _buttons[1];
-    stepForwardButton = _buttons[2];
-  }
-  
+
+  num get x => workspace.width - width;
+
+  num get y => 0;
+
+  num get height => workspace.height;
+
   
   void addBlock(Block block, int count) {
     slots.add(new Slot(block, workspace, count));
-  }
-  
-  
-  bool overlaps(Block block) {
-    return (block.centerY >= y);
   }
   
   
@@ -66,9 +54,14 @@ class Menu {
   }
 
 
-  Block getBlockByName(String name) {
+  bool isOverMenu(Block block) {
+    return (!block._inMenu && !block._wasInMenu && block.x + block.width * 0.75 >= x);
+  }
+
+
+  Block getBlockByAction(String action) {
     for (Slot slot in slots) {
-      if (slot.block.text == name) {
+      if (slot.block.action == action) {
         return slot.block;
       }
     }
@@ -76,77 +69,30 @@ class Menu {
   }
 
 
-
-  void _buttonAction(VCRButton button) { 
-    if (workspace != null && workspace.runtime != null) {
-      switch (button.shape) {
-        case VCRButtonShape.Play: 
-          workspace.runtime.play(); 
-          break;
-        case VCRButtonShape.Pause: 
-          workspace.runtime.pause(); 
-          break;
-        case VCRButtonShape.FastForward: 
-          workspace.runtime.fastForward(); 
-          break;
-        case VCRButtonShape.Restart: 
-          workspace.runtime.restart(); 
-          break;
-        case VCRButtonShape.StepForward:
-          workspace.runtime.stepForward();
-          break;
-        default: break;
-      }
-      if (workspace.runtime.isRunning) {
-        playButton.shape = VCRButtonShape.Pause;
-      } else {
-        playButton.shape = VCRButtonShape.Play;
-      }
+  void _resize(CanvasRenderingContext2D ctx) {
+    width = BLOCK_WIDTH * 1.5;
+    for (Slot slot in slots) {
+      width = max(width, slot.block._getNaturalWidth(ctx) + BLOCK_GUTTER * 2);
     }
   }
 
   
-  void draw(CanvasRenderingContext2D ctx) {
+  void draw(CanvasRenderingContext2D ctx, bool highlightTrash) {
+    _resize(ctx);
     ctx.save();
     {
-      ctx.fillStyle = background;
-      ctx.fillRect(x, y, w, h);
+      ctx.fillStyle = color;
+      ctx.fillRect(x, y, width, height);
+      if (highlightTrash) ctx.fillRect(x, y, width, height);
 
-      num ix = x + 25;
-      num iy = y + h/2;
+      num ix = x + BLOCK_GUTTER;
+      num iy = y + BLOCK_HEIGHT / 2;
       
       for (Slot slot in slots) {
         slot.x = ix;
-        slot.y = iy - slot.height / 2;
+        slot.y = iy;
         slot.draw(ctx);
-        ix += slot.width + 10;
-      }
-
-      num bspace = 45;
-      ix = x + w;
-      if (fastForwardButton.visible) ix -= bspace;
-
-      for (int i=_buttons.length-1; i>=0; i--) {
-        if (_buttons[i].visible) {
-          VCRButton button = _buttons[i];
-          ix -= bspace;
-          button.x = ix;
-          button.y = y + h/2 - button.width/2;
-          button.draw(ctx);
-        }
-      }
-
-      // draw fast-forward indicator
-      if (fastForwardButton.visible && workspace != null && workspace.runtime != null) {
-        if (workspace.runtime.play_state > 1) {
-          ctx.font = '400 18pt Nunito, sans-serif';
-          ctx.textAlign = 'left';
-          ctx.textBaseline = 'middle';
-          ctx.fillStyle = 'white';
-          String s = "x${workspace.runtime.play_state}";
-          //num fw = ctx.measureText(s).width + 14;
-          ctx.fillText(s, x + w - 50, y + h/2);
-        }
+        iy += BLOCK_HEIGHT * 1.5;
       }
     }
     ctx.restore();
@@ -157,7 +103,6 @@ class Menu {
 class Slot implements Touchable {
   
   Block block;
-  Block target = null;
   num x, y;
   
   CodeWorkspace workspace;
@@ -166,13 +111,13 @@ class Slot implements Touchable {
   
   
   Slot(this.block, this.workspace, this.count) {
-    block.inMenu = true;
+    block._inMenu = true;
     workspace.addTouchable(this);
   }
   
   
   bool isAvailable() {
-    return workspace.getBlockCount(block.text) < count;
+    return true;
   }
   
   
@@ -181,7 +126,14 @@ class Slot implements Touchable {
   
   
   void draw(CanvasRenderingContext2D ctx) {
-    int free = count - workspace.getBlockCount(block.text);
+    block.x = x;
+    block.y = y;
+    block._resizeChain(ctx, BLOCK_WIDTH);
+    block._drawBlock(ctx);
+    block._drawLabel(ctx);
+    block._drawOutline(ctx);
+    /*
+    int free = count - workspace.getBlockCount(block.action);
     if (free <= 0) {
       block.x = x.toDouble() - 1;
       block.y = y.toDouble() + 1;
@@ -193,6 +145,7 @@ class Slot implements Touchable {
         block.draw(ctx);
       }
     }
+    */
   }
   
   
@@ -201,180 +154,28 @@ class Slot implements Touchable {
   }
   
   
-  bool touchDown(Contact c) {
-    if (target == null && block.containsTouch(c) && isAvailable()) {
-      target = block.clone();
+  Touchable touchDown(Contact c) {
+    if (isAvailable()) {
+      Block target = block.clone();
+      target.x = block.x - 5;
+      target.y = block.y - 5;
+      target._wasInMenu = true;
       workspace._addBlock(target);
-      target.move(-2, -8);
-      target.touchDown(c);
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  
-  void touchUp(Contact c) {
-    if (target != null) {
-      target.touchUp(c);
-    }
-    target = null;
-  }
-  
-  
-  void touchDrag(Contact c) {
-    if (target != null) {
-      target.touchDrag(c);
-    }
-  }
-  
-  void touchSlide(Contact c) { }
-}
-
-
-enum VCRButtonShape { Play, Pause, FastForward, StepForward, Restart }
-
-
-class VCRButton implements Touchable {
-
-  Menu menu;
-
-  CodeWorkspace workspace;
-
-  num x = 0, y = 0, width;
-
-  VCRButtonShape shape = VCRButtonShape.Play;
-
-  bool _down = false;
-
-  bool visible = true;
-
-
-  VCRButton(this.menu, this.width, this.shape) {
-    this.workspace = menu.workspace;
-    workspace.addTouchable(this);
-  }
-
-
-  void draw(CanvasRenderingContext2D ctx) {
-    num bx = _down? x + 2 : x;
-    num by = _down? y + 2 : y;
-    ctx.save();
-    {
-      switch (shape) {
-        case VCRButtonShape.Play: _playPath(ctx, bx, by); break;
-        case VCRButtonShape.Pause: _pausePath(ctx, bx, by); break;
-        case VCRButtonShape.FastForward: _fastForwardPath(ctx, bx, by); break;
-        case VCRButtonShape.StepForward: _stepForwardPath(ctx, bx, by); break;
-        case VCRButtonShape.Restart: _restartPath(ctx, bx, by); break;
+      if (target is BeginBlock) {
+        for (ClauseBlock clause in target.clauses) {
+          workspace._addBlock(clause);
+        }
       }
-      if (!_down) {
-        ctx.shadowOffsetX = 2;
-        ctx.shadowOffsetY = 2;
-        ctx.shadowBlur = 4;
-        ctx.shadowColor = "rgba(0, 0, 0, 0.1)";
-      }
-      ctx.fillStyle = "rgba(255, 255, 255, ${_down ? 0.7 : 1})";
-      ctx.fill();
+
+      return target.touchDown(c);
     }
-    ctx.restore();
-  }
-
-
-  void _playPath(CanvasRenderingContext2D ctx, num bx, num by) {
-    num bw = width * 0.9;
-    bx = bx + width/2 - bw/2;
-    by = by + width/2 - bw/2;
-    ctx.beginPath();
-    ctx.moveTo(bx, by);
-    ctx.lineTo(bx + bw, by + bw/2);
-    ctx.lineTo(bx, by + bw);
-    ctx.closePath();
-  }
-
-
-  void _pausePath(CanvasRenderingContext2D ctx, num bx, num by) {
-    num margin = 2;
-    num bw = width * 0.28;
-    ctx.beginPath();
-    ctx.rect(bx + margin, by, bw, width);
-    ctx.rect(bx + width - bw - margin, by, bw, width);
-  }
-
-
-  void _fastForwardPath(CanvasRenderingContext2D ctx, num bx, num by) {
-    num bw = width * 0.55;
-    num bh = width * 0.7;
-    by = by + width/2 - bh/2;
-    ctx.beginPath();
-    ctx.moveTo(bx, by);
-    ctx.lineTo(bx + bw, by + bh/2);
-    ctx.lineTo(bx, by + bh);
-    ctx.closePath();
-    bx += width * 0.45;
-    ctx.moveTo(bx, by);
-    ctx.lineTo(bx + bw, by + bh/2);
-    ctx.lineTo(bx, by + bh);
-    ctx.closePath();
-  }
-
-
-  void _stepForwardPath(CanvasRenderingContext2D ctx, num bx, num by) {
-    num bw = width * 0.55;
-    num bh = width * 0.7;
-    by = by + width/2 - bh/2;
-    bx = bx + width/2 - bw/2;
-    num bar = width * 0.2;
-    ctx.beginPath();
-    ctx.moveTo(bx, by);
-    ctx.lineTo(bx + bw, by + bh/2);
-    ctx.lineTo(bx, by + bh);
-    ctx.closePath();
-    ctx.rect(bx + bw - 1, by, bar, bh);
-  }
-
-
-  void _restartPath(CanvasRenderingContext2D ctx, num bx, num by) {
-    num bw = width * 0.85;
-    num r1 = bw * 0.5;
-    num r2 = bw * 0.3;
-    num r3 = bw * 0.4;
-    num cx = bx + width/2;
-    num cy = by + width/2;
-    ctx.beginPath();
-    ctx.arc(cx, cy, r1, PI * 0.2, PI * 1.75, false);
-    ctx.lineTo(cx + r1 - 1, cy - r3 - 2);
-    ctx.lineTo(cx + r1 - 1, cy - 2);
-    ctx.lineTo(cx + r1 - r3 - 1, cy - 2);
-    ctx.arc(cx, cy, r2, PI * 1.75, PI * 0.2, true);
-    ctx.closePath();
+    return this;
   }
   
-
-  bool containsTouch(Contact c) {
-    return (c.touchX >= x - 10 && c.touchX <= x + width + 10 && 
-            c.touchY >= y - 30 && c.touchY <= y + width + 20);
-  }
-  
-  
-  bool touchDown(Contact c) {
-    _down = true;
-    workspace.draw();
-    return true;
-  }
-
-    
-  void touchUp(Contact c) {
-    _down = false;
-    if (containsTouch(c)) {
-      menu._buttonAction(this);
-    }
-    workspace.draw();
-  }
-  
+  void touchUp(Contact c) { }
   
   void touchDrag(Contact c) { }
   
-  void touchSlide(Contact c) { }  
+  void touchSlide(Contact c) { }
 }
 
