6da7dc245a088140fab7409e63e54446f14bed30
Michael Horn <michael-horn@northwestern.edu>
2017-10-05 22:15:14 -0500

    changed interface to work with parse trees; implemented plain formatter

--- lib/src/blocks/formatter.dart
+++ lib/src/blocks/formatter.dart
@@ -17,24 +17,71 @@ part of NetTango;
 
 
 abstract class CodeFormatter  {
+
+  String _indent = "  ";
   
-  /// destination language format 
-  String language = "NetLogo";
+  /// convert parse tree output from workspace into source code of a target output language
+  String format(var parseTree);
 
 
-  CodeFormatter(this.language);
-
-  String format(CodeWorkspace workspace);
+  void _formatIndent(StringBuffer out, int indent) {
+    for (int i=0; i<=indent; i++) out.write(_indent);
+  }
 }
 
 
 
+class PlainFormatter extends CodeFormatter {
+
+  String format(var parseTree) {
+    StringBuffer out = new StringBuffer();
+    for (var chain in parseTree["chains"]) {
+      _formatChain(out, chain, 0);
+      out.writeln();
+    }
+
+    return out.toString();
+  }
+
+
+  void _formatChain(StringBuffer out, var chain, int indent) {
+    for (var block in chain) {
+      _formatBlock(out, block, indent);
+      if (block["children"] is List) {
+        _formatChain(out, block["children"], indent+1);
+      }
+      if (block["clauses"] is List) {
+        for (var clause in block["clauses"]) {
+          _formatBlock(out, clause, indent);
+          if (clause["children"] is List) {
+            _formatChain(out, clause["children"], indent+1);
+          }
+        }
+      }
+    }
+  }
+
+
+  void _formatBlock(StringBuffer out, var block, int indent) {
+    _formatIndent(out, indent);
+    out.write(block["action"] + "( ");
+    if (block["params"] is List) {
+      for (var param in block["params"]) {
+        out.write("${param["value"]} ");
+      }
+    }
+    out.writeln(")");
+  }
+}
+
+
+/*
 class NetLogoFormatter extends CodeFormatter {
 
-  NetLogoFormatter() : super("NetLogo");
+  NetLogoFormatter();
 
 
-  String format(CodeWorkspace workspace) {
+  String format(var parseTree) {
     StringBuffer out = new StringBuffer();
     for (Block block in workspace.blocks) {
       if (block.type == "nlogo:procedure") {
@@ -92,9 +139,5 @@ class NetLogoFormatter extends CodeFormatter {
       }
     }
   }
-
-
-  void _formatIndent(StringBuffer out, Block block) {
-    for (int i=0; i<=block.indent; i++) out.write("  ");
-  }
 }
+*/
